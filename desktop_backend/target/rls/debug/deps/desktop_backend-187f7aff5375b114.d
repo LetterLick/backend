93ef7e7de5d54c1471a0cdc1af80e9c076fdf626
Tommy270502 <perrithomas@outlook.com>
2022-07-08 22:58:33 +0200

    Started Desktop Backend Project

---
+++ desktop_backend/target/rls/debug/deps/desktop_backend-187f7aff5375b114.d
@@ -0,0 +1,5 @@
+c:\Users\Thomas\Desktop\Rust-Lang\backend\desktop_backend\target\rls\debug\deps\desktop_backend-187f7aff5375b114.rmeta: src\main.rs
+
+c:\Users\Thomas\Desktop\Rust-Lang\backend\desktop_backend\target\rls\debug\deps\desktop_backend-187f7aff5375b114.d: src\main.rs
+
+src\main.rs:
---
+++ desktop_backend/target/rls/debug/deps/desktop_backend-e1b3e4a5349da3e0.d
@@ -0,0 +1,5 @@
+c:\Users\Thomas\Desktop\Rust-Lang\backend\desktop_backend\target\rls\debug\deps\desktop_backend-e1b3e4a5349da3e0.rmeta: src\main.rs
+
+c:\Users\Thomas\Desktop\Rust-Lang\backend\desktop_backend\target\rls\debug\deps\desktop_backend-e1b3e4a5349da3e0.d: src\main.rs
+
+src\main.rs:
